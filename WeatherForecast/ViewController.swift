3f74e395374ee1f03efd72e11340bc9153b9e808
Vasilii Pronin <letstalkaboutpork@gmail.com>
2022-02-03 17:26:22 +0300

    Initial Commit

---
+++ WeatherForecast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherForecast
+//
+//  Created by Василий Пронин on 03.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
