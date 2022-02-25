1f6f573dd97cf3f48cccf0f7a00b5a3e00d6f0d2
Vasilii Pronin <vesily92@gmail.com>
2022-02-25 15:24:07 +0300

    SceneDelegate fix

--- WeatherForecast/SupportingFiles/SceneDelegate.swift
+++ WeatherForecast/SupportingFiles/SceneDelegate.swift
@@ -14,9 +14,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene: windowScene)
-        window?.makeKeyAndVisible()
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
         window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
