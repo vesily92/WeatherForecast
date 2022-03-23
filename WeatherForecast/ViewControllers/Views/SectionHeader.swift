b7d6e3ddc08cd7fd2113ed23901678443f57ede6
Vasilii Pronin <vesily92@gmail.com>
2022-03-23 23:18:17 +0300

    Section header update (background view added)

--- WeatherForecast/ViewControllers/Views/SectionHeader.swift
+++ WeatherForecast/ViewControllers/Views/SectionHeader.swift
@@ -13,9 +13,11 @@ final class SectionHeader: UICollectionReusableView {
     let titleLabel = UILabel()
     let symbolView = UIImageView()
     let backgroundView = UIImageView()
+    let supportingBackgroundView = UIImageView()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
         
         titleLabel.font = .systemFont(ofSize: 12)
         titleLabel.textColor = .systemGray4
@@ -25,7 +27,11 @@ final class SectionHeader: UICollectionReusableView {
         
         backgroundView.backgroundColor = .systemGray2
         backgroundView.layer.cornerRadius = 12
+//        backgroundView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
         backgroundView.translatesAutoresizingMaskIntoConstraints = false
+        
+        supportingBackgroundView.backgroundColor = .systemGray4
+        supportingBackgroundView.translatesAutoresizingMaskIntoConstraints = false
         
         let headerTitleStackView = UIStackView(arrangedSubviews: [
             symbolView,
@@ -37,19 +43,34 @@ final class SectionHeader: UICollectionReusableView {
         headerTitleStackView.spacing = 4
         headerTitleStackView.translatesAutoresizingMaskIntoConstraints = false
         
+        addSubview(supportingBackgroundView)
         addSubview(backgroundView)
         addSubview(headerTitleStackView)
         
         NSLayoutConstraint.activate([
-            backgroundView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            backgroundView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            backgroundView.topAnchor.constraint(equalTo: topAnchor),
+            supportingBackgroundView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            supportingBackgroundView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            supportingBackgroundView.topAnchor.constraint(equalTo: topAnchor),
+            supportingBackgroundView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16),
+
+            backgroundView.leadingAnchor.constraint(equalTo: supportingBackgroundView.leadingAnchor),
+            backgroundView.trailingAnchor.constraint(equalTo: supportingBackgroundView.trailingAnchor),
+            backgroundView.topAnchor.constraint(equalTo: supportingBackgroundView.topAnchor),
             backgroundView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+//            backgroundView.leadingAnchor.constraint(equalTo: leadingAnchor),
+//            backgroundView.trailingAnchor.constraint(equalTo: trailingAnchor),
+//            backgroundView.topAnchor.constraint(equalTo: topAnchor),
+//            backgroundView.bottomAnchor.constraint(equalTo: bottomAnchor),
 
             headerTitleStackView.leadingAnchor.constraint(equalTo: backgroundView.leadingAnchor, constant: 16),
             headerTitleStackView.widthAnchor.constraint(greaterThanOrEqualToConstant: 50),
-            headerTitleStackView.topAnchor.constraint(equalTo: backgroundView.topAnchor, constant: 12)
+            headerTitleStackView.topAnchor.constraint(equalTo: backgroundView.topAnchor, constant: 12),
+            headerTitleStackView.bottomAnchor.constraint(equalTo: backgroundView.bottomAnchor, constant: -12)
         ])
+    }
+    
+    override func apply(_ layoutAttributes: UICollectionViewLayoutAttributes) {
         
     }
     
