252a92ee7fc9fb04602418c4940d7772f208d299
Vasilii Pronin <vesily92@gmail.com>
2022-03-28 22:12:19 +0300

    New fetching functions created

--- WeatherForecast/Services/NetworkManager.swift
+++ WeatherForecast/Services/NetworkManager.swift
@@ -23,14 +23,47 @@ class NetworkManager {
     
     enum SectionType {
 
-        case current(Current)
-        case hourly(Hourly)
-        case daily(Daily)
+        case current
+        case hourly
+        case daily
     }
  
     static let shared = NetworkManager()
 
     private init() {}
+    
+    func fetchData(_ sectionType: SectionType, with completion: @escaping (AnyHashable) -> Void) {
+        let urlString = "https://api.openweathermap.org/data/2.5/onecall?lat=59.9311&lon=30.3609&exclude=minutely&appid=\(apiKey)&units=metric"
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            
+            do {
+                let data = try JSONDecoder().decode(ForecastData.self, from: data)
+                switch sectionType {
+                case .current:
+                    DispatchQueue.main.async {
+                        completion(data.current)
+                    }
+                case .hourly:
+                    DispatchQueue.main.async {
+                        completion(data.hourly)
+                    }
+                case .daily:
+                    DispatchQueue.main.async {
+                        completion(data.daily)
+                    }
+                }
+            } catch let error {
+                print(error)
+            }
+        }.resume()
+    }
 
     func fetchCurrentData(with completion: @escaping (Current) -> Void) {
         let urlString = "https://api.openweathermap.org/data/2.5/onecall?lat=59.9311&lon=30.3609&exclude=minutely&appid=\(apiKey)&units=metric"
@@ -42,8 +75,8 @@ class NetworkManager {
                 print(error?.localizedDescription ?? "No error description")
                 return
             }
-            print("-------------------data: \(data)")
-            print(String(data: data, encoding: .utf8)!)
+//            print("-------------------data: \(data)")
+//            print(String(data: data, encoding: .utf8)!)
             do {
                 let forecastData = try JSONDecoder().decode(ForecastData.self, from: data)
 //                guard let forecast = ForecastData.CurrentData(model: forecastData) else { return }
@@ -67,8 +100,8 @@ class NetworkManager {
                 print(error?.localizedDescription ?? "No error description")
                 return
             }
-            print("-------------------data: \(data)")
-            print(String(data: data, encoding: .utf8)!)
+//            print("-------------------data: \(data)")
+//            print(String(data: data, encoding: .utf8)!)
             do {
                 let forecastData = try JSONDecoder().decode(ForecastData.self, from: data)
 //                guard let forecast = ForecastData.CurrentData(model: forecastData) else { return }
@@ -92,8 +125,8 @@ class NetworkManager {
                 print(error?.localizedDescription ?? "No error description")
                 return
             }
-            print("-------------------data: \(data)")
-            print(String(data: data, encoding: .utf8)!)
+//            print("-------------------data: \(data)")
+//            print(String(data: data, encoding: .utf8)!)
             do {
                 let forecastData = try JSONDecoder().decode(ForecastData.self, from: data)
 //                guard let forecast = ForecastData.CurrentData(model: forecastData) else { return }
