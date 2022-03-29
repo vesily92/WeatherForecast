340989ac84c1d38a4dc2cb3f8172bcb8a3161002
Vasilii Pronin <vesily92@gmail.com>
2022-03-29 22:14:28 +0300

    NetworkManager updated

--- WeatherForecast/Services/NetworkManager.swift
+++ WeatherForecast/Services/NetworkManager.swift
@@ -20,19 +20,13 @@ class NetworkManager {
         case cityName(city: String)
         case coordinates(latitude: CLLocationDegrees, longitude: CLLocationDegrees)
     }
-    
-    enum SectionType {
-
-        case current
-        case hourly
-        case daily
-    }
  
     static let shared = NetworkManager()
 
     private init() {}
     
-    func fetchData(_ sectionType: SectionType, with completion: @escaping (AnyHashable) -> Void) {
+    func fetchData(_ sectionType: Section, with completion: @escaping (AnyHashable) -> Void) {
+        //var weatherData: [AnyHashable] = []
         let urlString = "https://api.openweathermap.org/data/2.5/onecall?lat=59.9311&lon=30.3609&exclude=minutely&appid=\(apiKey)&units=metric"
         guard let url = URL(string: urlString) else {
             return
@@ -44,20 +38,41 @@ class NetworkManager {
             }
             
             do {
-                let data = try JSONDecoder().decode(ForecastData.self, from: data)
+                let forecastData = try JSONDecoder().decode(ForecastData.self, from: data)
                 switch sectionType {
-                case .current:
-                    DispatchQueue.main.async {
-                        completion(data.current)
-                    }
                 case .hourly:
                     DispatchQueue.main.async {
-                        completion(data.hourly)
+                        completion(forecastData.hourly)
                     }
                 case .daily:
                     DispatchQueue.main.async {
-                        completion(data.daily)
+                        completion(forecastData.daily)
                     }
+                }
+            } catch let error {
+                print(error)
+            }
+        }.resume()
+    }
+    
+    func fetchForecastData(with completion: @escaping (ForecastData) -> Void) {
+        let urlString = "https://api.openweathermap.org/data/2.5/onecall?lat=59.9311&lon=30.3609&exclude=minutely&appid=\(apiKey)&units=metric"
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+//            print("-------------------data: \(data)")
+//            print(String(data: data, encoding: .utf8)!)
+            do {
+                let forecastData = try JSONDecoder().decode(ForecastData.self, from: data)
+//                guard let forecast = ForecastData.CurrentData(model: forecastData) else { return }
+//                print("forecast data: \(forecast)")
+                DispatchQueue.main.async {
+                    completion(forecastData)
                 }
             } catch let error {
                 print(error)
@@ -83,56 +98,6 @@ class NetworkManager {
 //                print("forecast data: \(forecast)")
                 DispatchQueue.main.async {
                     completion(forecastData.current)
-                }
-            } catch let error {
-                print(error)
-            }
-        }.resume()
-    }
-    
-    func fetchHourlyData(with completion: @escaping ([Hourly]) -> Void) {
-        let urlString = "https://api.openweathermap.org/data/2.5/onecall?lat=59.9311&lon=30.3609&exclude=minutely&appid=\(apiKey)&units=metric"
-        guard let url = URL(string: urlString) else {
-            return
-        }
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let data = data else {
-                print(error?.localizedDescription ?? "No error description")
-                return
-            }
-//            print("-------------------data: \(data)")
-//            print(String(data: data, encoding: .utf8)!)
-            do {
-                let forecastData = try JSONDecoder().decode(ForecastData.self, from: data)
-//                guard let forecast = ForecastData.CurrentData(model: forecastData) else { return }
-//                print("forecast data: \(forecast)")
-                DispatchQueue.main.async {
-                    completion(forecastData.hourly)
-                }
-            } catch let error {
-                print(error)
-            }
-        }.resume()
-    }
-    
-    func fetchDailyData(with completion: @escaping ([Daily]) -> Void) {
-        let urlString = "https://api.openweathermap.org/data/2.5/onecall?lat=59.9311&lon=30.3609&exclude=minutely&appid=\(apiKey)&units=metric"
-        guard let url = URL(string: urlString) else {
-            return
-        }
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let data = data else {
-                print(error?.localizedDescription ?? "No error description")
-                return
-            }
-//            print("-------------------data: \(data)")
-//            print(String(data: data, encoding: .utf8)!)
-            do {
-                let forecastData = try JSONDecoder().decode(ForecastData.self, from: data)
-//                guard let forecast = ForecastData.CurrentData(model: forecastData) else { return }
-//                print("forecast data: \(forecast)")
-                DispatchQueue.main.async {
-                    completion(forecastData.daily)
                 }
             } catch let error {
                 print(error)
