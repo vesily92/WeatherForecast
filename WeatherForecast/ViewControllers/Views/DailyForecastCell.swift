6bbd6261e02c6cdf144c57bf9891edce01fe4d49
Vasilii Pronin <vesily92@gmail.com>
2022-03-29 22:14:04 +0300

    DailyForecastCell updated. Implemented weather icons day/night change

--- WeatherForecast/ViewControllers/Views/DailyForecastCell.swift
+++ WeatherForecast/ViewControllers/Views/DailyForecastCell.swift
@@ -11,7 +11,6 @@ class DailyForecastCell: UICollectionViewCell, SelfConfiguringCell {
     
     static let reuseIdentifier = "DailyForecastCell"
     
-    lazy private var dateLabel = UILabel()
     lazy private var weekdayLabel = UILabel()
     lazy private var highestTemperatureLabel = UILabel()
     lazy private var lowestTemperatureLabel = UILabel()
@@ -20,16 +19,13 @@ class DailyForecastCell: UICollectionViewCell, SelfConfiguringCell {
     
     var collectionView: UICollectionView!
     
-    private let symbolConfig = UIImage.SymbolConfiguration(font: .systemFont(ofSize: 16))
+    private let symbolConfig = UIImage.SymbolConfiguration(font: .systemFont(ofSize: 18))
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         weekdayLabel.font = .systemFont(ofSize: 18, weight: .bold)
         weekdayLabel.textColor = .white
-        
-        dateLabel.font = .systemFont(ofSize: 12, weight: .light)
-        dateLabel.textColor = .white
         
         highestTemperatureLabel.font = .systemFont(ofSize: 18, weight: .bold)
         highestTemperatureLabel.textColor = .white
@@ -42,25 +38,14 @@ class DailyForecastCell: UICollectionViewCell, SelfConfiguringCell {
         
         weatherIconView.preferredSymbolConfiguration = .preferringMulticolor()
         weatherIconView.contentMode = .scaleAspectFit
-        
-        let dateStackView = UIStackView(arrangedSubviews: [
-            weekdayLabel,
-            dateLabel
-        ])
-        dateStackView.axis = .vertical
-        dateStackView.alignment = .leading
-        
+
         let weatherIconStackView = UIStackView(arrangedSubviews: [
             weatherIconView,
             probabilityOfPrecipitationLabel
         ])
         weatherIconStackView.axis = .vertical
         
-        let emptyStackView = UIStackView()
-        
         let temperatureStackView = UIStackView(arrangedSubviews: [
-            weatherIconStackView,
-            emptyStackView,
             highestTemperatureLabel,
             lowestTemperatureLabel
         ])
@@ -70,12 +55,14 @@ class DailyForecastCell: UICollectionViewCell, SelfConfiguringCell {
         
         let mainStackView = UIStackView(arrangedSubviews: [
             weekdayLabel,
-//            dateStackView,
+            weatherIconStackView,
             temperatureStackView
         ])
         mainStackView.distribution = .equalCentering
         mainStackView.axis = .horizontal
         mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        contentView.addSubview(mainStackView)
         
         setupConstraints(for: mainStackView)
     }
@@ -83,15 +70,6 @@ class DailyForecastCell: UICollectionViewCell, SelfConfiguringCell {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-//    func configure(with forecast: AnyHashable) {
-//        guard let model = forecast as? Daily.Diffable else { return }
-//        dateLabel.text = model.date
-//        weekdayLabel.text = model.weekday
-//        highestTemperatureLabel.text = model.highestTemperature
-//        lowestTemperatureLabel.text = model.lowestTemperature
-//        weatherIconView.image = UIImage(systemName: model.systemNameString)
-//    }
     
     func configure(with forecast: AnyHashable) {
         guard let model = forecast as? Daily else { return }
@@ -101,31 +79,19 @@ class DailyForecastCell: UICollectionViewCell, SelfConfiguringCell {
         let minTemp = model.temperature.min
         let pop = model.pop
         let icon = model.weather.first!.systemNameString
+        
+        var isNow: Bool {
+            return DateFormatter.verify(.day, from: time)
+        }
 
-        dateLabel.text = DateManager.shared.defineDate(withUnixTime: time, andDateFormat: .date)
-        weekdayLabel.text = DateManager.shared.defineDate(withUnixTime: time, andDateFormat: .weekday).capitalized
+        weekdayLabel.text = isNow ? "Today" : DateFormatter.format(unixTime: time, to: .weekday)
         highestTemperatureLabel.text = format(input: maxTemp, modifier: true)
         lowestTemperatureLabel.text = format(input: minTemp, modifier: true)
         probabilityOfPrecipitationLabel.text = format(input: pop)
         weatherIconView.image = UIImage(systemName: icon, withConfiguration: symbolConfig)
     }
-    
-//    func configure(with forecast: AnyHashable) {
-//        guard let forecast = forecast as? ForecastData.DailyData else { return }
-//
-//        DispatchQueue.main.async {
-//            self.dateLabel.text = forecast.date
-//            self.weekdayLabel.text = forecast.weekday
-//            self.highestTemperatureLabel.text = forecast.highestTemperature
-//            self.lowestTemperatureLabel.text = forecast.lowestTemperature
-//            self.probabilityOfPrecipitationLabel.text = forecast.probabilityOfPrecipitation
-//            self.weatherIconView.image = UIImage(systemName: forecast.systemNameString, withConfiguration: self.symbolConfig)
-//        }
-//    }
-    
+ 
     fileprivate func setupConstraints(for uiView: UIView) {
-        contentView.addSubview(uiView)
-        
         NSLayoutConstraint.activate([
             uiView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
             uiView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
