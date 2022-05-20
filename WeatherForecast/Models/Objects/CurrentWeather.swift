61247f12386372498893264af7b126063f3a2210
Vasilii Pronin <vesily92@gmail.com>
2022-05-20 15:29:48 +0300

    CurrentWeather object created

--- WeatherForecast/Models/Objects/CurrentWeather.swift
+++ WeatherForecast/Models/Objects/CurrentWeather.swift
@@ -8,50 +8,115 @@
 import Foundation
 
 struct CurrentWeather: Codable {
-    
     let cityName: String
-    let description: String
-    
-    let temperature: Double
-    var temperatureString: String {
-        return String(format: "%.0f", temperature.rounded(.toNearestOrAwayFromZero)) + " °"
-    }
-    
+    let time: Int
+    let sunrise: Int
+    let sunset: Int
+    let temp: Double
     let feelsLike: Double
-    var feelsLikeString: String {
-        return "Feels like: " + String(format: "%.0f", feelsLike.rounded(.toNearestOrAwayFromZero)) + " °"
-    }
-    
+    let description: String
+    let visibility: Int
+    let pressure: Int
+    let humidity: Int
+    let timezone: Int
+//    let windSpeed: Int
+//    let windDirection: Int
     let conditionCode: Int
+    
+    var timeString: String {
+        return DateFormatter.format(time, to: .detailed, withTimeZoneOffset: timezone)
+    }
+    var sunriseString: String {
+        return DateFormatter.format(sunrise, to: .sunrise, withTimeZoneOffset: timezone)
+    }
+    var sunsetString: String {
+        return DateFormatter.format(sunset, to: .sunrise, withTimeZoneOffset: timezone)
+    }
+    var tempString: String {
+        return temp.displayTemp()
+    }
+    var feelsLikeString: String {
+        return "Feels like: " + feelsLike.displayTemp()
+    }
+    var descriptionString: String {
+        return description.capitalized
+    }
+    var visibilityString: String {
+        return "\(visibility)"
+    }
+    var pressureString: String {
+        return "\(pressure)"
+    }
+    var humidityString: String {
+        return "\(humidity)"
+    }
+//    var windSpeedString: String {
+//        return "\(windSpeed)"
+//    }
+//    var windDirectionString: String {
+//        return "\(windDirection)"
+//    }
+    var sunIsUp: Bool = false
     var systemNameString: String {
         switch conditionCode {
-        case 200...232: return "cloud.bolt.rain.fill" //"11d"
-        case 300...321: return "cloud.drizzle.fill" //"09d"
-        case 500...531: return "cloud.heavyrain.fill" //"10d"
-        case 600...622: return "cloud.snow.fill" //"13d"
-        case 700...781: return "cloud.fog.fill" //"50d"
-        case 800: return "sun.max.fill" //"01d"
-        case 801...804: return "cloud.sun.fill" //"04d"
+        case 200...232: return "cloud.bolt.rain.fill"
+        case 300...321: return "cloud.drizzle.fill"
+        case 500...531: return "cloud.heavyrain.fill"
+        case 600...622: return "cloud.snow.fill"
+        case 700...781: return "cloud.fog.fill"
+        case 800: return sunIsUp ? "sun.max.fill" : "moon.stars.fill"
+        case 801...804: return sunIsUp ? "cloud.sun.fill" : "cloud.moon.fill"
         default: return "nosign"
         }
     }
     
     init?(model: CurrentWeatherData) {
+        
+        if model.weather.first!.icon.hasSuffix("d") {
+            sunIsUp = true
+        }
+        
         cityName = model.name
-        temperature = model.main.temp
-        description = model.weather.first!.description.capitalized
+        time = model.dt
+        sunrise = model.sys.sunrise
+        sunset = model.sys.sunset
+        temp = model.main.temp
         feelsLike = model.main.feelsLike
+        description = model.weather.first!.description
+        visibility = model.visibility
+        pressure = model.main.pressure
+        humidity = model.main.humidity
+        timezone = model.timezone
+//        windSpeed = model.wind.speed ?? 0
+//        windDirection = model.wind.deg ?? 0
         conditionCode = model.weather.first!.id
     }
 }
 
 extension CurrentWeather: Hashable {
-
+    
     var id: UUID {
         return UUID()
     }
     
+    static func == (lhs: CurrentWeather, rhs: CurrentWeather) -> Bool {
+        return lhs.id == rhs.id
+    }
+    
     func hash(into hasher: inout Hasher) {
         hasher.combine(id)
+        hasher.combine(cityName)
+        hasher.combine(time)
+        hasher.combine(sunrise)
+        hasher.combine(sunset)
+        hasher.combine(temp)
+        hasher.combine(feelsLike)
+        hasher.combine(description)
+        hasher.combine(visibility)
+        hasher.combine(pressure)
+        hasher.combine(humidity)
+//        hasher.combine(windSpeed)
+//        hasher.combine(windDirection)
+        hasher.combine(conditionCode)
     }
 }
