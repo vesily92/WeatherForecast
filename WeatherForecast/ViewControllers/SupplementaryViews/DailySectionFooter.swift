910b61750fa7892700d61c7ff1b8be7c33142a4d
Vasilii Pronin <vesily92@gmail.com>
2022-06-16 22:28:44 +0400

    DailySectionFooter created

---
+++ WeatherForecast/ViewControllers/SupplementaryViews/DailySectionFooter.swift
@@ -0,0 +1,13 @@
+//
+//  DailySectionFooter.swift
+//  WeatherForecast
+//
+//  Created by Василий Пронин on 16.06.2022.
+//
+
+import UIKit
+
+class DailySectionFooter: UICollectionReusableView {
+    static let reuseIdentifier = "DailySectionFooter"
+    
+}
