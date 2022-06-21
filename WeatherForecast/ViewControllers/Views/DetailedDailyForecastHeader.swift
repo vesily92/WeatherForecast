27ed43e11564faf05cd9df648cc8cddb621451ca
Vasilii Pronin <vesily92@gmail.com>
2022-06-21 21:57:23 +0400

    DetailedDailyForecastHeader created

---
+++ WeatherForecast/ViewControllers/Views/DetailedDailyForecastHeader.swift
@@ -0,0 +1,14 @@
+//
+//  DetailedDailyForecastHeader.swift
+//  WeatherForecast
+//
+//  Created by Василий Пронин on 20.06.2022.
+//
+
+import UIKit
+
+class DetailedDailyForecastHeader: UICollectionReusableView {
+    static let reuseIdentifier = "DetailedDailyForecastHeader"
+    
+    
+}
