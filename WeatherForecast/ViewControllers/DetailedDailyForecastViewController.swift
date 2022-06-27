96e3da1a0cbc0bbf19d66e0f7cef1f21771c5dd1
Vasilii Pronin <vesily92@gmail.com>
2022-06-27 23:00:15 +0400

    DetailedDailyForecastVC updated

--- WeatherForecast/ViewControllers/DetailedDailyForecastViewController.swift
+++ WeatherForecast/ViewControllers/DetailedDailyForecastViewController.swift
@@ -10,21 +10,28 @@ import UIKit
 class DetailedDailyForecastViewController: UIViewController {
     
     private enum DetailedDailySection: Int, CaseIterable {
-//        case dailyTemp
-        case detailedInfo
+        case dayPicker
+        case dailyTempInfo
+        case uviAndHumidityInfo
+        case pressureAndWindInfo
     }
     
-    private typealias DataSource = UICollectionViewDiffableDataSource<DetailedDailySection, Daily>
-    private typealias Snapshot = NSDiffableDataSourceSnapshot<DetailedDailySection, Daily>
+    private struct CategorisedDailyItems: Hashable {
+        let details: Daily
+        let category: DetailedDailySection
+    }
+    
+    private typealias DataSource = UICollectionViewDiffableDataSource<DetailedDailySection, CategorisedDailyItems>
+    private typealias Snapshot = NSDiffableDataSourceSnapshot<DetailedDailySection, CategorisedDailyItems>
     
     private var collectionView: UICollectionView!
     private var dataSource: DataSource?
-    
     private var forecastData: ForecastData? {
         didSet {
             dataSource?.apply(makeSnapshot(), animatingDifferences: false)
         }
     }
+    private let inset: CGFloat = 16
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,17 +53,16 @@ class DetailedDailyForecastViewController: UIViewController {
         view.addSubview(collectionView)
         
         collectionView.register(
-            DetailedDailyTempCell.self,
-            forCellWithReuseIdentifier: DetailedDailyTempCell.reuseIdentifier
+            DayPickerCell.self,
+            forCellWithReuseIdentifier: DayPickerCell.reuseIdentifier
         )
         collectionView.register(
-            DetailedDailyForecastCell.self,
-            forCellWithReuseIdentifier: DetailedDailyForecastCell.reuseIdentifier
+            DailyTempCell.self,
+            forCellWithReuseIdentifier: DailyTempCell.reuseIdentifier
         )
         collectionView.register(
-            DetailedDailyForecastHeader.self,
-            forSupplementaryViewOfKind: DetailedDailyForecastHeader.reuseIdentifier,
-            withReuseIdentifier: DetailedDailyForecastHeader.reuseIdentifier
+            MeteorologicInfoCell.self,
+            forCellWithReuseIdentifier: MeteorologicInfoCell.reuseIdentifier
         )
     }
     
@@ -67,22 +73,19 @@ class DetailedDailyForecastViewController: UIViewController {
             }
             
             switch section {
-//            case .dailyTemp:
-//                return self.createTempSection(using: section)
-            case .detailedInfo:
-                return self.createDetailedInfoSection(using: section)
+            case .dayPicker:
+                return self.createDayPickerSection(using: section)
+            case .dailyTempInfo:
+                return self.createDailyTempSection(using: section)
+            case .uviAndHumidityInfo, .pressureAndWindInfo:
+                return self.createMeteorologicInfoSection(using: section)
             }
         }
-        
-        let detailedDailyForecastHeader = createGlobalHeader()
-        let config = UICollectionViewCompositionalLayoutConfiguration()
-        config.boundarySupplementaryItems = [detailedDailyForecastHeader]
-        layout.configuration = config
-        
+
         return layout
     }
     
-    private func createTempSection(using: DetailedDailySection) -> NSCollectionLayoutSection {
+    private func createDailyTempSection(using: DetailedDailySection) -> NSCollectionLayoutSection {
         let itemSize = NSCollectionLayoutSize(
             widthDimension: .fractionalWidth(1.0),
             heightDimension: .fractionalHeight(1.0)
@@ -90,54 +93,54 @@ class DetailedDailyForecastViewController: UIViewController {
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
         let groupSize = NSCollectionLayoutSize(
             widthDimension: .fractionalWidth(1.0),
-            heightDimension: .estimated(150)
+            heightDimension: .fractionalWidth(1 / 2)
         )
         let group = NSCollectionLayoutGroup.vertical(
             layoutSize: groupSize,
             subitems: [item]
         )
         let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: inset, leading: inset, bottom: inset / 2, trailing: inset)
         return section
     }
     
-    private func createDetailedInfoSection(using: DetailedDailySection) -> NSCollectionLayoutSection {
-        let fraction: CGFloat = 1/2
-        let inset: CGFloat = 8
-        
+    private func createDayPickerSection(using: DetailedDailySection) -> NSCollectionLayoutSection {
         let itemSize = NSCollectionLayoutSize(
-            widthDimension: .fractionalWidth(fraction),
+            widthDimension: .estimated(68),
             heightDimension: .fractionalHeight(1.0)
         )
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        item.contentInsets = NSDirectionalEdgeInsets(top: inset, leading: inset, bottom: inset, trailing: inset)
         let groupSize = NSCollectionLayoutSize(
-            widthDimension: .fractionalWidth(1.0),
-            heightDimension: .fractionalWidth(fraction)
+            widthDimension: .estimated(68),
+            heightDimension: .estimated(90.0)
         )
         let group = NSCollectionLayoutGroup.horizontal(
             layoutSize: groupSize,
             subitems: [item]
         )
-//        group.contentInsets = NSDirectionalEdgeInsets(top: inset, leading: inset, bottom: inset, trailing: inset)
-        
         let section = NSCollectionLayoutSection(group: group)
-        section.contentInsets = NSDirectionalEdgeInsets(top: inset, leading: inset, bottom: inset, trailing: inset)
+        section.orthogonalScrollingBehavior = .continuous
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 16)
         return section
     }
     
-    private func createGlobalHeader() -> NSCollectionLayoutBoundarySupplementaryItem {
-        let globalHeaderSize = NSCollectionLayoutSize(
+    private func createMeteorologicInfoSection(using: DetailedDailySection) -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(
             widthDimension: .fractionalWidth(1.0),
-            heightDimension: .estimated(200)
+            heightDimension: .fractionalHeight(1.0)
         )
-        let globalHeader = NSCollectionLayoutBoundarySupplementaryItem(
-            layoutSize: globalHeaderSize,
-            elementKind: DetailedDailyForecastHeader.reuseIdentifier,
-            alignment: .top
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalWidth(1/2)
         )
-        globalHeader.pinToVisibleBounds = true
-        
-        return globalHeader
+        let group = NSCollectionLayoutGroup.vertical(
+            layoutSize: groupSize,
+            subitems: [item]
+        )
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: inset / 2, leading: inset, bottom: inset / 2, trailing: inset)
+        return section
     }
     
     private func createDataSource() {
@@ -145,33 +148,37 @@ class DetailedDailyForecastViewController: UIViewController {
             guard let section = DetailedDailySection(rawValue: indexPath.section) else {
                 fatalError("Unknown section kind")
             }
-//            guard let offset = self.forecastData?.timezoneOffset else {
-//                fatalError("No date")
-//            }
-            
+            guard let offset = self.forecastData?.timezoneOffset else {
+                fatalError("No date")
+            }
+
             switch section {
-//            case .dailyTemp:
-//                guard let cell = self.collectionView.dequeueReusableCell(withReuseIdentifier: DetailedDailyTempCell.reuseIdentifier, for: indexPath) as? DetailedDailyTempCell else {
-//                    fatalError("Unable to dequeue cell")
-//                }
-//                cell.configure(with: forecast)
-//                return cell
-            case .detailedInfo:
-                guard let cell = self.collectionView.dequeueReusableCell(withReuseIdentifier: DetailedDailyForecastCell.reuseIdentifier, for: indexPath) as? DetailedDailyForecastCell else {
+            case .dayPicker:
+                guard let cell = self.collectionView.dequeueReusableCell(withReuseIdentifier: DayPickerCell.reuseIdentifier, for: indexPath) as? DayPickerCell else {
+                    fatalError("Unable to dequeue DayPickerCell")
+                }
+                cell.configure(with: forecast.details)
+                return cell
+            case .dailyTempInfo:
+                guard let cell = self.collectionView.dequeueReusableCell(withReuseIdentifier: DailyTempCell.reuseIdentifier, for: indexPath) as? DailyTempCell else {
                     fatalError("Unable to dequeue cell")
                 }
-                switch indexPath.item {
-                case 0: cell.configure(for: .temp, with: forecast)
-                case 1: cell.configure(for: .feelsLike, with: forecast)
-                case 2: cell.configure(for: .uvi, with: forecast)
-                case 3: cell.configure(for: .humidity, with: forecast)
-                case 4: cell.configure(for: .pressure, with: forecast)
-                case 5: cell.configure(for: .wind, with: forecast)
-                default: return cell
-                }
+                cell.configure(with: forecast.details, andTimeZoneOffset: offset)
                 return cell
+            case .uviAndHumidityInfo:
+                guard let meteorologicCell = self.collectionView.dequeueReusableCell(withReuseIdentifier: MeteorologicInfoCell.reuseIdentifier, for: indexPath) as? MeteorologicInfoCell else {
+                    fatalError("Unable to dequeue MeteorologicInfoCell")
+                }
+                meteorologicCell.configure(for: .uviAndHumidity, with: forecast.details, andTimeZoneOffset: offset)
+                return meteorologicCell
+            case .pressureAndWindInfo:
+                guard let meteorologicCell = self.collectionView.dequeueReusableCell(withReuseIdentifier: MeteorologicInfoCell.reuseIdentifier, for: indexPath) as? MeteorologicInfoCell else {
+                    fatalError("Unable to dequeue MeteorologicInfoCell")
+                }
+                meteorologicCell.configure(for: .pressureAndWind, with: forecast.details, andTimeZoneOffset: offset)
+                return meteorologicCell
             }
-            
+
         })
         dataSource?.supplementaryViewProvider = { collectionView, kind, indexPath in
             guard let globalHeader = collectionView.dequeueReusableSupplementaryView(
@@ -181,22 +188,27 @@ class DetailedDailyForecastViewController: UIViewController {
             ) as? DetailedDailyForecastHeader else {
                 fatalError("Unknown header kind")
             }
-            
+
             return globalHeader
         }
     }
     
     private func makeSnapshot() -> Snapshot {
         var snapshot = Snapshot()
-        
+
         guard let forecastData = forecastData else {
             fatalError()
         }
         
         snapshot.appendSections(DetailedDailySection.allCases)
-//        snapshot.appendItems(forecastData.daily, toSection: .dailyTemp)
-        snapshot.appendItems(forecastData.daily, toSection: .detailedInfo)
         
+        for dailyData in forecastData.daily {
+            snapshot.appendItems([CategorisedDailyItems(details: dailyData, category: .dayPicker)], toSection: .dayPicker)
+        }
+        snapshot.appendItems([CategorisedDailyItems(details: forecastData.daily.first!, category: .dailyTempInfo)], toSection: .dailyTempInfo)
+        snapshot.appendItems([CategorisedDailyItems(details: forecastData.daily.first!, category: .uviAndHumidityInfo)], toSection: .uviAndHumidityInfo)
+        snapshot.appendItems([CategorisedDailyItems(details: forecastData.daily.first!, category: .pressureAndWindInfo)], toSection: .pressureAndWindInfo)
+
         return snapshot
     }
 }
