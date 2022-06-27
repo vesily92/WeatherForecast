236b2066e7290a808439a5bb71a8322b723ef8ba
Vasilii Pronin <vesily92@gmail.com>
2022-06-27 22:59:28 +0400

    DetailedForecastHeader created

--- WeatherForecast/ViewControllers/Views/DetailedDailyForecastHeader.swift
+++ WeatherForecast/ViewControllers/Views/DetailedDailyForecastHeader.swift
@@ -10,5 +10,24 @@ import UIKit
 class DetailedDailyForecastHeader: UICollectionReusableView {
     static let reuseIdentifier = "DetailedDailyForecastHeader"
     
+    private lazy var heading = UILabel(.sf20SemiboldWhite)
+    private lazy var weekday = UILabel(.sf20MediumWhite)
+    private lazy var date = UILabel(.sf20SemiboldWhite)
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        heading.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(with model: Daily) {
+        weekday.text = DateFormatter.format(model.dt, to: .weekdayShort)
+        date.text = DateFormatter.format(model.dt, to: .dateShort)
+    }
 }
