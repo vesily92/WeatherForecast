7113cdb4e2b7dc44858dd4ffa76b875b30fd842e
Vasilii Pronin <vesily92@gmail.com>
2022-07-07 18:31:58 +0400

    SectionHeader updated

--- WeatherForecast/ViewControllers/SupplementaryViews/SectionHeader.swift
+++ WeatherForecast/ViewControllers/SupplementaryViews/SectionHeader.swift
@@ -14,32 +14,32 @@ final class SectionHeader: UICollectionReusableView {
     private lazy var iconView = UIImageView()
     
     private let symbolConfig = UIImage.SymbolConfiguration(font: .systemFont(ofSize: 20))
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-
-//        iconView.contentMode = .scaleAspectFit
-//        iconView.tintColor = .black
-//        iconView.alpha = 0.3
-        
-        let headerTitleStackView = UIStackView(arrangedSubviews: [
-            iconView,
-            titleLabel
-        ])
-        headerTitleStackView.axis = .horizontal
-        headerTitleStackView.alignment = .firstBaseline
-        
-        headerTitleStackView.spacing = 4
-        headerTitleStackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        addSubview(headerTitleStackView)
-        
-        NSLayoutConstraint.activate([
-            headerTitleStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
-            headerTitleStackView.widthAnchor.constraint(greaterThanOrEqualToConstant: 50),
-            headerTitleStackView.topAnchor.constraint(equalTo: topAnchor, constant: 12)
-        ])
-    }
+//
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//
+////        iconView.contentMode = .scaleAspectFit
+////        iconView.tintColor = .black
+////        iconView.alpha = 0.3
+//
+////        let headerTitleStackView = UIStackView(arrangedSubviews: [
+////            iconView,
+////            titleLabel
+////        ])
+////        headerTitleStackView.axis = .horizontal
+////        headerTitleStackView.alignment = .firstBaseline
+////
+////        headerTitleStackView.spacing = 4
+////        headerTitleStackView.translatesAutoresizingMaskIntoConstraints = false
+////
+////        addSubview(headerTitleStackView)
+////
+////        NSLayoutConstraint.activate([
+////            headerTitleStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+////            headerTitleStackView.widthAnchor.constraint(greaterThanOrEqualToConstant: 50),
+////            headerTitleStackView.topAnchor.constraint(equalTo: topAnchor, constant: 12)
+////        ])
+//    }
 
     func configureForAlertSection(with forecast: ForecastData) {
         guard let alerts = forecast.alerts?.count,
@@ -53,11 +53,24 @@ final class SectionHeader: UICollectionReusableView {
         titleLabel.textColor = .white
         
         iconView.contentMode = .scaleAspectFit
-        iconView.preferredSymbolConfiguration = symbolConfig
+        iconView.preferredSymbolConfiguration = UIImage.SymbolConfiguration(font: .systemFont(ofSize: 20))
         iconView.tintColor = .white
         
         titleLabel.text = areFew ? "\(event.capitalized) & \(alerts - 1) More" : "\(event)"
         iconView.image = UIImage(systemName: "exclamationmark.triangle.fill")
+        setupConstraintsForNormalState()
+    }
+    
+    func configureForLargeState(with section: Section) {
+        titleLabel.font = .systemFont(ofSize: 26, weight: .semibold)
+        titleLabel.textColor = .white
+        
+        iconView.contentMode = .scaleAspectFit
+        iconView.preferredSymbolConfiguration = UIImage.SymbolConfiguration(font: .systemFont(ofSize: 26))
+        iconView.tintColor = .white
+        titleLabel.text = section.headerTitle
+        iconView.image = UIImage(systemName: section.headerIcon)
+        setupConstraintsForLargeState()
     }
 
     func configure(with section: Section) {
@@ -71,9 +84,45 @@ final class SectionHeader: UICollectionReusableView {
         
         titleLabel.text = section.headerTitle
         iconView.image = UIImage(systemName: section.headerIcon)
+        setupConstraintsForNormalState()
+    }
+//    
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//    
+    fileprivate func setupConstraintsForNormalState() {
+        let headerTitleStackView = UIStackView(arrangedSubviews: [
+            iconView,
+            titleLabel
+        ])
+        headerTitleStackView.axis = .horizontal
+        headerTitleStackView.alignment = .firstBaseline
+        headerTitleStackView.spacing = 4
+        headerTitleStackView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(headerTitleStackView)
+        
+        NSLayoutConstraint.activate([
+            headerTitleStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            headerTitleStackView.widthAnchor.constraint(greaterThanOrEqualToConstant: 50),
+            headerTitleStackView.topAnchor.constraint(equalTo: topAnchor, constant: 12)
+        ])
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    fileprivate func setupConstraintsForLargeState() {
+        let headerTitleStackView = UIStackView(arrangedSubviews: [
+            iconView,
+            titleLabel
+        ])
+        headerTitleStackView.axis = .horizontal
+        headerTitleStackView.alignment = .center
+        headerTitleStackView.spacing = 4
+        headerTitleStackView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(headerTitleStackView)
+        
+        NSLayoutConstraint.activate([
+            headerTitleStackView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            headerTitleStackView.topAnchor.constraint(equalTo: topAnchor, constant: 16)
+        ])
     }
 }
