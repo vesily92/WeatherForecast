27267c82374140047bd0aae151024a89d5c227f6
Vasilii Pronin <vesily92@gmail.com>
2022-09-20 00:03:27 +0400

    UILabel extension updated

--- WeatherForecast/Extensions/UILabel + Extension.swift
+++ WeatherForecast/Extensions/UILabel + Extension.swift
@@ -10,70 +10,44 @@ import UIKit
 extension UILabel {
     
     enum LabelType {
-        case heading16SemiboldBlack
-        case sf26SemiboldWhite
-        case sf20SemiboldWhite
-        case sf20SemiboldBlack
-        case sf20MediumWhite
-        case sf20ThinWhite
-        case sf16SemiboldWhite
-        case sf16RegularWhite
-        case sf12SemiboldTeal
-        case sf12RegularBlack
+        case largeTitle36
+        case globalTemperature
+        case mainText20
+        case specificationText16
+        case largeText26
+        case smallText12
+        
+        enum Color {
+            case gray
+            case teal
+        }
     }
-    
-    convenience init(_ labelType: LabelType, alpha: CGFloat = 1) {
+
+    convenience init(_ labelType: LabelType, color: LabelType.Color? = nil) {
         self.init()
         
         switch labelType {
-        case .heading16SemiboldBlack:
+        case .largeTitle36:
+            self.font = .systemFont(ofSize: 36, weight: .semibold)
+        case .globalTemperature:
+            self.font = .systemFont(ofSize: 54, weight: .regular)
+        case .mainText20:
+            self.font = .systemFont(ofSize: 20, weight: .semibold)
+        case .specificationText16:
             self.font = .systemFont(ofSize: 16, weight: .semibold)
-            self.textColor = .black
-            self.alpha = 0.3
-        case .sf26SemiboldWhite:
+        case .largeText26:
             self.font = .systemFont(ofSize: 26, weight: .semibold)
-            self.textColor = .white
-            self.alpha = alpha
-        case .sf20SemiboldWhite:
-            self.font = .systemFont(ofSize: 20, weight: .semibold)
-            self.textColor = .white
-            self.alpha = alpha
-        case .sf20SemiboldBlack:
-            self.font = .systemFont(ofSize: 20, weight: .semibold)
-            self.textColor = .black
-            self.alpha = 0.3
-        case .sf20MediumWhite:
-            self.font = .systemFont(ofSize: 20, weight: .medium)
-            self.textColor = .white
-            self.alpha = alpha
-        case .sf20ThinWhite:
-            self.font = .systemFont(ofSize: 20, weight: .thin)
-            self.textColor = .white
-            self.alpha = alpha
-        case .sf16SemiboldWhite:
-            self.font = .systemFont(ofSize: 16, weight: .semibold)
-            self.textColor = .white
-            self.alpha = alpha
-        case .sf16RegularWhite:
-            self.font = .systemFont(ofSize: 16, weight: .regular)
-            self.textColor = .white
-            self.alpha = alpha
-        case .sf12SemiboldTeal:
+        case .smallText12:
             self.font = .systemFont(ofSize: 12, weight: .semibold)
-            self.textColor = .systemTeal
-            self.alpha = alpha
-        case .sf12RegularBlack:
-            self.font = .systemFont(ofSize: 12)
-            self.textColor = .black
-            self.alpha = 0.3
         }
-    }
-    
-    convenience init(fontSize: CGFloat, weight: UIFont.Weight = .semibold, color: UIColor = .white, alpha: CGFloat = 1) {
-        self.init()
-
-        self.font = .systemFont(ofSize: fontSize, weight: weight)
-        self.textColor = color
-        self.alpha = alpha
+        
+        switch color {
+        case .gray:
+            self.textColor = .gray
+        case .teal:
+            self.textColor = .systemTeal
+        case .none:
+            self.textColor = .white
+        }
     }
 }
