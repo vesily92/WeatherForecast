c140d91d40c2927683f40d5320a2810abb9f2b8e
Vasilii Pronin <vesily92@gmail.com>
2022-09-20 09:47:28 +0400

    AlertCell updated

--- WeatherForecast/ViewControllers/Views/AlertCell.swift
+++ WeatherForecast/ViewControllers/Views/AlertCell.swift
@@ -8,48 +8,62 @@
 import UIKit
 
 class AlertCell: UICollectionViewCell, SelfConfiguringCell {
+    
     static let reuseIdentifier = "AlertCell"
     
-    private lazy var descriptionLabel = UILabel(.sf16RegularWhite)
-    private lazy var iconView = UIImageView()
-    private lazy var seeMoreButton = UIButton(.seeMoreButton)
+    weak var coordinator: Coordinator?
+
+    private lazy var descriptionLabel = UILabel(.specificationText16)
+    private lazy var seeMoreButton: UIButton = {
+        let button = UIButton()
+        
+        var config = UIButton.Configuration.plain()
+        config.title = "See More"
+        config.titleAlignment = .leading
+        config.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer { incoming in
+            var outgoing = incoming
+            outgoing.font = .systemFont(ofSize: 16, weight: .semibold)
+            return outgoing
+        }
+        config.image = UIImage(systemName: "chevron.right")
+        config.imagePlacement = .trailing
+        config.imagePadding = 20
+        config.baseForegroundColor = .white
+        
+        button.configuration = config
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
 
         descriptionLabel.numberOfLines = 0
         descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        seeMoreButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        let imageView = UIImageView()
-        imageView.image = UIImage(
-            systemName: "chevron.right",
-            withConfiguration: UIImage.SymbolConfiguration(font: .systemFont(ofSize: 16))
-        )
-        imageView.tintColor = .white
-        
+
         let separator = UIView(frame: .zero)
         separator.backgroundColor = .white
         separator.alpha = 0.7
         separator.translatesAutoresizingMaskIntoConstraints = false
-        
-        let buttonStackView = UIStackView(arrangedSubviews: [
-            seeMoreButton,
-            imageView
-        ])
-        buttonStackView.axis = .horizontal
-        buttonStackView.distribution = .equalCentering
-        buttonStackView.alignment = .firstBaseline
-        buttonStackView.translatesAutoresizingMaskIntoConstraints = false
-        
+
+        contentView.addSubview(seeMoreButton)
+        contentView.addSubview(separator)
         contentView.addSubview(descriptionLabel)
         
         NSLayoutConstraint.activate([
             descriptionLabel.topAnchor.constraint(equalTo: contentView.topAnchor),
             descriptionLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
             descriptionLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
-            descriptionLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+            
+            separator.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            separator.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            separator.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 10),
+            separator.heightAnchor.constraint(equalToConstant: 1),
+            
+            seeMoreButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            seeMoreButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            seeMoreButton.topAnchor.constraint(equalTo: separator.bottomAnchor),
+            seeMoreButton.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
         ])
     }
     
@@ -60,15 +74,14 @@ class AlertCell: UICollectionViewCell, SelfConfiguringCell {
     func configure(with forecast: AnyHashable, andTimezoneOffset offset: Int) {
         guard let forecast = forecast as? Alert else { return }
         
-        if !forecast.senderName.isEmpty && !forecast.alertDescription.isEmpty {
-            descriptionLabel.text = "\(forecast.senderName): \(forecast.event)\n\(forecast.alertDescription)"
-        } else if !forecast.senderName.isEmpty {
+        if !forecast.senderName.isEmpty {
             descriptionLabel.text = "\(forecast.senderName): \(forecast.event)"
-        } else if !forecast.alertDescription.isEmpty {
-            descriptionLabel.text = "\(forecast.event)\n\(forecast.alertDescription)"
         } else {
             descriptionLabel.text = forecast.event
         }
-
+    }
+    
+    @objc func didTapButton() {
+//        coordinator?.eventOccurred(with: .buttonTapped)
     }
 }
