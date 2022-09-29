518162e7e9eec46d1d80db0c621fd32f66757f20
Vasilii Pronin <vesily92@gmail.com>
2022-09-29 15:26:04 +0400

    LocationManager updated

--- WeatherForecast/Services/LocationManager.swift
+++ WeatherForecast/Services/LocationManager.swift
@@ -7,71 +7,43 @@
 
 import CoreLocation
 
-struct Location {
-    var cityName: String
+struct Location: Hashable {
+    var cityName: String?
     var latitude: CLLocationDegrees?
     var longitude: CLLocationDegrees?
 }
 
 class LocationManager: NSObject, CLLocationManagerDelegate {
-//
-//    enum QueryType {
-//        case city(city: String)
-//        case coordinates(coordinates: CLLocation)
-//    }
     
     static let shared = LocationManager()
     
     let manager = CLLocationManager()
-    
     var completion: ((CLLocation) -> Void)?
     
-    public func getUserLocation(completion: @escaping ((CLLocation) -> Void)) {
+    private override init() {}
+    
+    func getUserLocation(completion: @escaping ((CLLocation) -> Void)) {
         self.completion = completion
         manager.requestWhenInUseAuthorization()
         manager.delegate = self
         manager.startUpdatingLocation()
     }
     
-    
-    
-    
-    
-    
-    public func getLocations(with forecastData: [ForecastData], completion: @escaping (([Location]?) -> Void)) {
-        let geocoder = CLGeocoder()
-        
-        var coordinates = CLLocation()
-        
-        forecastData.forEach { forecast in
-            let locationCoordinates = CLLocation(latitude: forecast.lat, longitude: forecast.lon)
-            coordinates = locationCoordinates
-            
-            geocoder.reverseGeocodeLocation(coordinates, preferredLocale: .init(identifier: "en")) { placemarks, error in
-                guard let placemarks = placemarks, error == nil else {
-                    completion([])
-                    return
-                }
-                let models: [Location] = placemarks.compactMap { placemark in
-                    var name = ""
-                    if let locality = placemark.locality {
-                        name += locality
-                    }
-                    return Location(cityName: name)
-                }
-                
-                completion(models)
-            }
+    func getLocations(from forecastData: [ForecastData], completion: @escaping (([Location]?) -> Void)) {
+        var locations: [Location] = []
+        var data = forecastData
+        data.removeFirst()
+        data.forEach { forecast in
+            let location = Location(
+                latitude: forecast.lat,
+                longitude: forecast.lon
+            )
+            locations.append(location)
         }
+        completion(locations)
     }
-    
-    
-    
-    
-    
-    
-    
-    public func getLocations(with query: String, completion: @escaping (([Location]?) -> Void)) {
+
+    func getLocations(with query: String, completion: @escaping (([Location]?) -> Void)) {
         let geocoder = CLGeocoder()
         geocoder.geocodeAddressString(query, in: nil, preferredLocale: .init(identifier: "en")) { placemarks, error in
             guard let placemarks = placemarks, error == nil else {
@@ -107,7 +79,7 @@ class LocationManager: NSObject, CLLocationManagerDelegate {
         }
     }
     
-    public func getLocationName(with coordinates: CLLocation, completion: @escaping ((Location?) -> Void)) {
+    func getLocationName(with coordinates: CLLocation, completion: @escaping ((Location?) -> Void)) {
         let geocoder = CLGeocoder()
         geocoder.reverseGeocodeLocation(coordinates, preferredLocale: .init(identifier: "en")) { placemarks, error in
             guard let placemark = placemarks?.first, error == nil else {
@@ -129,33 +101,6 @@ class LocationManager: NSObject, CLLocationManagerDelegate {
             completion(location)
         }
     }
-    
-    public func getLocationCoordinates(with cityName: String, completion: @escaping ((Location?) -> Void)) {
-        let geocoder = CLGeocoder()
-        geocoder.geocodeAddressString(cityName) { placemarks, error in
-            guard let placemark = placemarks?.first, error == nil else {
-                completion(nil)
-                return
-            }
-
-            guard let coordinates = placemark.location else {
-                completion(nil)
-                return
-            }
-            
-            let location = Location(
-                cityName: cityName,
-                latitude: coordinates.coordinate.latitude,
-                longitude: coordinates.coordinate.longitude
-            )
-            
-            completion(location)
-        }
-    }
-    
-//    public func resolveLocation(for result: Location, completion: @escaping (CLLocationDegrees) -> Void) {
-//        
-//    }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else { return }
