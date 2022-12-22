d9c3460f3638d595d30c1c63db3a006c9d387e7d
Vasilii Pronin <vesily92@gmail.com>
2022-12-22 18:59:52 +0400

    Services update

--- WeatherForecast/Services/LocationManager.swift
+++ WeatherForecast/Services/LocationManager.swift
@@ -18,69 +18,94 @@ class LocationManager: NSObject, CLLocationManagerDelegate {
     static let shared = LocationManager()
     
     let manager = CLLocationManager()
-    var completion: ((CLLocation) -> Void)?
+    var completion: ((Double, Double) -> Void)?
     
     private override init() {}
     
-    func getUserLocation(completion: @escaping ((CLLocation) -> Void)) {
+    func getUserLocation(completion: @escaping ((Double, Double) -> Void)) {
         self.completion = completion
         manager.requestWhenInUseAuthorization()
         manager.delegate = self
         manager.startUpdatingLocation()
     }
     
-    func getLocations(from forecastData: [ForecastData], completion: @escaping (([Location]?) -> Void)) {
-        var locations: [Location] = []
-        var data = forecastData
-        data.removeFirst()
-        data.forEach { forecast in
-            let location = Location(
-                latitude: forecast.lat,
-                longitude: forecast.lon
-            )
-            locations.append(location)
-        }
-        completion(locations)
-    }
+//    func getLocations(from forecastData: [ForecastData], completion: @escaping (([Location]?) -> Void)) {
+//        var locations: [Location] = []
+//        var data = forecastData
+//        data.removeFirst()
+//        data.forEach { forecast in
+//            let location = Location(
+//                latitude: forecast.lat,
+//                longitude: forecast.lon
+//            )
+//            locations.append(location)
+//        }
+//        completion(locations)
+//    }
 
-    func getLocations(with query: String, completion: @escaping (([Location]?) -> Void)) {
-        let geocoder = CLGeocoder()
-        geocoder.geocodeAddressString(query, in: nil, preferredLocale: .init(identifier: "en")) { placemarks, error in
-            guard let placemarks = placemarks, error == nil else {
-                completion([])
-                return
-            }
-            
-            let models: [Location] = placemarks.compactMap { placemark in
-                var name = ""
-                
-                if let cityName = placemark.locality {
-                    name += cityName
-                    
-                    if let adminArea = placemark.administrativeArea {
-                        name += ", \(adminArea)"
-                    }
-                    
-                    if let country = placemark.country {
-                        name += ", \(country)"
-                    }
-                }
-                
-                let result = Location(
-                    cityName: name,
-                    latitude: placemark.location?.coordinate.latitude,
-                    longitude: placemark.location?.coordinate.longitude
-                )
-                
-                return result
-            }
-            
-            completion(models)
-        }
-    }
+//    func getLocations(with query: String, completion: @escaping (([Location]?) -> Void)) {
+//        let geocoder = CLGeocoder()
+//        geocoder.geocodeAddressString(query, in: nil, preferredLocale: .init(identifier: "en")) { placemarks, error in
+//            guard let placemarks = placemarks, error == nil else {
+//                completion([])
+//                return
+//            }
+//
+//            let models: [Location] = placemarks.compactMap { placemark in
+//                var name = ""
+//
+//                if let cityName = placemark.locality {
+//                    name += cityName
+//
+//                    if let adminArea = placemark.administrativeArea {
+//                        name += ", \(adminArea)"
+//                    }
+//
+//                    if let country = placemark.country {
+//                        name += ", \(country)"
+//                    }
+//                }
+//
+//                let result = Location(
+//                    cityName: name,
+//                    latitude: placemark.location?.coordinate.latitude,
+//                    longitude: placemark.location?.coordinate.longitude
+//                )
+//
+//                return result
+//            }
+//
+//            completion(models)
+//        }
+//    }
     
-    func getLocationName(with coordinates: CLLocation, completion: @escaping ((Location?) -> Void)) {
+//    func getLocationName(with coordinates: CLLocation, completion: @escaping ((Location?) -> Void)) {
+//        let geocoder = CLGeocoder()
+//        geocoder.reverseGeocodeLocation(coordinates, preferredLocale: .init(identifier: "en")) { placemarks, error in
+//            guard let placemark = placemarks?.first, error == nil else {
+//                completion(nil)
+//                return
+//            }
+//            
+//            guard let locality = placemark.locality else {
+//                completion(nil)
+//                return
+//            }
+//            
+//            let location = Location(
+//                cityName: locality,
+//                latitude: coordinates.coordinate.latitude,
+//                longitude: coordinates.coordinate.longitude
+//            )
+//            
+//            completion(location)
+//        }
+//    }
+    
+    func getLocationName(withLat latitude: Double, andLon longitude: Double, completion: @escaping ((Location?) -> Void)) {
         let geocoder = CLGeocoder()
+        let coordinates = CLLocation(latitude: latitude, longitude: longitude)
+        
         geocoder.reverseGeocodeLocation(coordinates, preferredLocale: .init(identifier: "en")) { placemarks, error in
             guard let placemark = placemarks?.first, error == nil else {
                 completion(nil)
@@ -104,8 +129,9 @@ class LocationManager: NSObject, CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else { return }
-        
-        completion?(location)
+        let latitude = location.coordinate.latitude
+        let longitude = location.coordinate.longitude
+        completion?(latitude, longitude)
         manager.stopUpdatingLocation()
     }
     
--- WeatherForecast/Services/NetworkManager.swift
+++ WeatherForecast/Services/NetworkManager.swift
@@ -6,28 +6,14 @@
 //
 
 import Foundation
-import CoreLocation
 
 class NetworkManager {
     
-    enum RequestType {
-        case cityName(city: String)
-        case coordinates(latitude: CLLocationDegrees, longitude: CLLocationDegrees)
-    }
-    
-    enum DataType {
-        case alertsData
-        case hourlyData
-        case dailyData
-    }
-
     static let shared = NetworkManager()
-    
-//    var onCompletion: ((CurrentWeather) -> Void)?
-    
+            
     private init() {}
     
-    func fetchOneCallData(withLatitude latitude: CLLocationDegrees, longitude: CLLocationDegrees, andCompletion completion: @escaping (ForecastData) -> Void) {
+    func fetchOneCallData(withLatitude latitude: Double, longitude: Double, andCompletion completion: @escaping (ForecastData) -> Void) {
         let urlString = "https://api.openweathermap.org/data/2.5/onecall?lat=\(latitude)&lon=\(longitude)&exclude=minutely&appid=\(apiKey)&units=metric&lang=en"
         
         guard let url = URL(string: urlString) else { return }
@@ -53,43 +39,29 @@ class NetworkManager {
         task.resume()
     }
     
-//    func fetchCurrentWeatherData(forRequestType requestType: RequestType) {
-//        var urlString = ""
-//        switch requestType {
-//        case .cityName(let city):
-//            urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(apiKey)&units=metric"
-//        case .coordinates(let latitude, let longitude):
-//            urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(apiKey)&units=metric"
-//        }
-//        performRequest(withURLString: urlString)
-//    }
-    
-//    fileprivate func performRequest(withURLString urlString: String) {
-//        guard let url = URL(string: urlString) else { return }
-//        let session = URLSession(configuration: .default)
-//        let task = session.dataTask(with: url) { data, response, error in
-//            guard let data = data else {
-//                print(error?.localizedDescription ?? "No error description")
-//                return
-//            }
-//            if let currentWeather = self.parseJSON(withData: data) {
-//                self.onCompletion?(currentWeather)
-//            }
-//        }
-//        task.resume()
-//    }
-    
-//    fileprivate func parseJSON(withData data: Data) -> CurrentWeather? {
-//        let decoder = JSONDecoder()
-//        do {
-//            let currentWeatherData = try decoder.decode(CurrentWeatherData.self, from: data)
-//            guard let currentWeather = CurrentWeather(model: currentWeatherData) else {
-//                return nil
-//            }
-//            return currentWeather
-//        } catch let error as NSError {
-//            print(String(describing: error))
-//        }
-//        return nil
-//    }
+    func fetchCoordinates(with query: String, completion: @escaping ([GeocodingData]) -> Void) {
+        let urlString = "https://api.openweathermap.org/geo/1.0/direct?q=\(query)&limit=\(5)&appid=\(apiKey)"
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: url) { data, response, error in
+            guard let data = data, let response = response else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            
+            guard url == response.url else { return }
+//            print(String(data: data, encoding: .utf8)!)
+            do {
+                let locations = try JSONDecoder().decode([GeocodingData].self, from: data)
+                DispatchQueue.main.async {
+                    completion(locations)
+                }
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        task.resume()
+    }
 }
